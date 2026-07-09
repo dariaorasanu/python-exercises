08ff171ced96490d82d061cd015cf0b891471684
Daria Orasanu <dariaorasanu3@gmail.com>
2026-07-09 18:50:38 +0300

    add exercise5

---
+++ Laborator1/ex5.py
@@ -0,0 +1,13 @@
+'''
+5. Write a function that checks whether a string contains special characters (\r, \t, \n, \a, \b, \f, \v)
+'''
+
+def contains_special_characters (text):
+    special_characters = ['\r', '\t', '\n', '\a', '\b', '\f', '\v']
+    for character in special_characters:
+        if character in text:
+            return True
+    return False
+
+
+print (contains_special_characters('am\trezolvat\nexercitiile'))
