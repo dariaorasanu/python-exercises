7d69de8b47a15a8e847cc7ead85feecd49ed1555
Daria Orasanu <dariaorasanu3@gmail.com>
2026-07-13 13:00:49 +0300

    update exercice 1

--- Laborator2/ex1.py
+++ Laborator2/ex1.py
@@ -1,16 +1,17 @@
-'''
+"""
 1.	Sa se scrie o functie care sa returneze o lista cu primele n numere din sirul lui Fibonacci.
-'''
+"""
 
-def fibonnacci_first_n (n):
+
+def fibonnacci_first_n(n):
     if n == 1:
         return [0]
     fib_list = [0, 1]
     i = 2
-    while (i < n):
+    while i < n:
         fib_list.append(fib_list[i - 2] + fib_list[i - 1])
         i = i + 1
     return fib_list
 
-print (fibonnacci_first_n(9))
+print(fibonnacci_first_n(9))
 
