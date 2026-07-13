f64e0226125abeeddbaa2eecac7a9dff33b507df
Daria Orasanu <dariaorasanu3@gmail.com>
2026-07-13 11:10:16 +0300

    update exercise 1

--- Laborator1/ex1.py
+++ Laborator1/ex1.py
@@ -1,16 +1,18 @@
-'''
+"""
 1.  Find the largest common divisor of multiple numbers.
 Define a function with variable number of parameters to resolve this.
-'''
+"""
 
-def largest_common_divisor_of2 (number1, number2):
+
+def largest_common_divisor_of2(number1, number2):
     while number2 != 0:
         rest = number1 % number2
         number1 = number2
         number2 = rest
     return number1
 
-def largest_common_divisor (*numbers):
+
+def largest_common_divisor(*numbers):
     largest_divisor = largest_common_divisor_of2(numbers[0], numbers[1])
     for number in numbers:
         largest_divisor = largest_common_divisor_of2(largest_divisor,number)
@@ -18,6 +20,8 @@ def largest_common_divisor (*numbers):
     return largest_divisor
 
 print(largest_common_divisor(24,16,44,4,8))
+
+
 
 
 
