5380a906537f8fd5910e232d9af89e2280795fb4
Daria Orasanu <dariaorasanu3@gmail.com>
2026-07-13 11:29:22 +0300

    update exercise 3

--- Laborator1/ex3.py
+++ Laborator1/ex3.py
@@ -1,27 +1,32 @@
-'''
+"""
 3. Scrieti o functie care returneaza numarul de cuvinte care exista intr-un string. Cuvintele sunt separate de spatii, si/sau semne de punctuatie (, ;, ? ! . )
 Intre 2 cuvinte pot aparea in orice combinatie spatii si smne de punctuatie
-'''
+"""
+
+
 def is_letter(letter):
     letter = letter.lower()
     if 'a' <= letter <= 'z':
         return True
     return False
 
+
 def is_separator(letter):
-    if letter == ' ' or letter == ',' or letter == ';' or letter == '?' or letter == '!' or letter == '.':
+    if letter in ' ,.;?!':
         return True
     return False
 
-def words_count (string):
+
+def words_count(text):
     words = 0
-    for i in range(0, len(string)):
-        if i == 0 and is_letter(string[i]):
+    for i in range(0, len(text)):
+        if i == 0 and is_letter(text[i]):
             words = words + 1
-        if is_letter(string[i]) == True and  is_separator(string[i - 1]) == True:
+        if is_letter(text[i]) and is_separator(text[i - 1]):
             words = words + 1
     return words
 
-print (words_count(' ; aici,am!   patru  .  ;  cuvinte?'))
+
+print(words_count(' ; aici,am!   patru  .  ;  cuvinte?'))
 #fara cuvinte:
-print (words_count('  ;? '))
+print(words_count('  ;? '))
