d5912b2175b4f2fbd5f20f89c2025f69d83b5066
Daria Orasanu <dariaorasanu3@gmail.com>
2026-07-13 13:15:11 +0300

    update exercise 5

--- Laborator2/ex5.py
+++ Laborator2/ex5.py
@@ -1,8 +1,10 @@
-'''
+"""
 5.	Sa se scrie o functie care primeste ca parametru o lista x, si un numar k. Sa se returneze o lista cu tuple care sa reprezinte combinari de len(x) luate cate k din lista x.
 Exemplu: pentru lista x = [1,2,3,4] si k = 3 se va returna [(1, 2, 3), (1, 2, 4), (1, 3, 4), (2, 3, 4)].
-'''
-def combinations (x, k):
+"""
+
+
+def combinations(x, k):
     if k == 1:
         return x
     result =[]
@@ -22,4 +24,4 @@ def combinations (x, k):
     backtracking(0, [])
     return result
 
-print (combinations([1,2,3,4], 3))
+print(combinations([1,2,3,4], 3))
