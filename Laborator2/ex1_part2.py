b4d13cc00416a99ed66f5e5384372bd9e4e6265e
Daria Orasanu <dariaorasanu3@gmail.com>
2026-07-13 13:02:33 +0300

    update exercice 1 part2

--- Laborator2/ex1_part2.py
+++ Laborator2/ex1_part2.py
@@ -1,11 +1,13 @@
-'''
+"""
 1*. In cazul sirului Fibonacci fiecare termen este suma precedentilor 2.
 Sa se scrie o functie care ia ca parametru 2 numere intregi, num_terms, n.
 Num_terms va indica cati termeni vor fi calculati din urmatorul sir.
 Sirul este asemanator Fibonacci cu mentiunea ca fiecare termen este suma ultimilor n termeni.
 Sirul initial este format din n-1 0-uri si un 1. (e.g. n=5 sirul initial este 0 0 0 0 1)
-'''
-def fibonnacci_extended (num_terms, n):
+"""
+
+
+def fibonnacci_extended(num_terms, n):
     fib_list = []
     for i in range (1, n):
         fib_list.append(0)
@@ -14,8 +16,9 @@ def fibonnacci_extended (num_terms, n):
         return fib_list[:num_terms]
     count = len(fib_list)
     while count < num_terms:
-        fib_list.append(sum(fib_list[count - n: count]))
+        fib_list.append(sum(fib_list[count - n:count]))
         count = count + 1
     return fib_list
 
-print (fibonnacci_extended(10, 5))
+print(fibonnacci_extended(10, 5))
+
