7e1c7b6336872757c68a17c608561f1ba9d38240
Daria Orasanu <dariaorasanu3@gmail.com>
2026-07-13 13:24:44 +0300

    update exercise 9

--- Laborator2/ex9.py
+++ Laborator2/ex9.py
@@ -1,10 +1,12 @@
-'''
+"""
 9.	Să se scrie o funție ce va ordona o listă de tuple de string-uri în funcție de al 3-lea caracter al celui de-al 2-lea element din tuplă.
 Exemplu: [('abc', 'bcd'), ('abc', 'zza')] ==> [('abc', 'zza'), ('abc', 'bcd')]
-'''
+"""
 
-def sort_string_tuples (string_list):
-    sorted_list = sorted(string_list, key = lambda i : i[1][2])
+
+def sort_string_tuples(string_list):
+    sorted_list = sorted(string_list, key = lambda i: i[1][2])
     return sorted_list
 
-print (sort_string_tuples([('avc', 'bcd'), ('abc', 'zza')]))
+print(sort_string_tuples([('avc', 'bcd'), ('abc', 'zza')]))
+
