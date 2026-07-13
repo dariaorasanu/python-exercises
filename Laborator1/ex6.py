5d92c3fd7a635406d3837acfad40c5544200bba6
Daria Orasanu <dariaorasanu3@gmail.com>
2026-07-13 11:45:45 +0300

    update exercice 6

--- Laborator1/ex6.py
+++ Laborator1/ex6.py
@@ -1,15 +1,17 @@
-'''
+"""
 6. Write a function that converts a string of characters written in UpperCamelCase into snake_case
-'''
+"""
 
 def convert_style(camel_case_string):
     snake_case_string = camel_case_string[0].lower()
     for i in range(1, len(camel_case_string)):
         if camel_case_string[i].isupper():
-            snake_case_string += '_' + camel_case_string[i].lower()
+            #concatenare cu format string
+            snake_case_string += f'_{camel_case_string[i].lower()}'
         else:
             snake_case_string += camel_case_string[i]
 
     return snake_case_string
 
-print (convert_style('UpperCamelCase'))
+print(convert_style('UpperCamelCase'))
+
