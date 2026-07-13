26d25cfb379a210de065c92bbb682eb6844151a7
Daria Orasanu <dariaorasanu3@gmail.com>
2026-07-13 13:12:56 +0300

    update exercise 3

--- Laborator2/ex3.py
+++ Laborator2/ex3.py
@@ -1,8 +1,10 @@
-'''
+"""
 3.	Fie un tuplu (x,y) reprezentarea unui punct intr-un sistem cartezian. Sa se scrie o functie care primeste ca parametru
 o lista de puncte si returneaza o lista de tuple (a,b,c) unice care reprezinta dreptele unice determinate de acele puncte
 ( (a,b,c) corespunde dreptei ax + by + c = 0).
-'''
+"""
+
+
 def get_line(point1, point2):
     x1 = point1[0]
     y1 = point1[1]
@@ -13,12 +15,14 @@ def get_line(point1, point2):
     c = x1 * y2 - x2 * y1
     return a, b, c
 
-def cmmdc (number1, number2):
+
+def cmmdc(number1, number2):
     while number2:
         rest = number1 % number2
         number1 = number2
         number2 = rest
     return number1
+
 
 def refactorize(a, b, c):
     divide_number = cmmdc(abs(a),cmmdc(abs(b),abs(c)))
@@ -36,7 +40,9 @@ def refactorize(a, b, c):
     elif a == 0 and b == 0 and c < 0:
         c = -c
     return a, b, c
-def get_all_unique_lines (points):
+
+
+def get_all_unique_lines(points):
     lines = []
     for i in range (0, len(points) - 1):
         for j in range (i + 1, len(points)):
@@ -46,4 +52,4 @@ def get_all_unique_lines (points):
                 lines.append(line)
     return lines
 
-print (get_all_unique_lines([(0, 0), (1, 1), (2, 2), (3, 3)]))
+print(get_all_unique_lines([(0, 0), (1, 1), (2, 2), (3, 3)]))
