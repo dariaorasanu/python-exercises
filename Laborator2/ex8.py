cb568fb0013aad93ad5d3eb6473adf52ca4b4d4c
Daria Orasanu <dariaorasanu3@gmail.com>
2026-07-13 13:23:18 +0300

    update exercise 8

--- Laborator2/ex8.py
+++ Laborator2/ex8.py
@@ -1,12 +1,13 @@
-'''
+"""
 8.	Sa se scrie o functie care primeste un numar variabil de liste si returneaza o lista de tuple astfel:
 primul tuplu sa contina primele elemente din liste, al doilea element sa contina elementele de pe pozitia 2 din liste, etc.
 Ex: pentru listele [1,2,3], [5,6,7], ["a", "b", "c"] se va returna: [(1,5,"a"), (2,6,"b"), (3,7,"c")].
 Observatie: In cazul in care listele primite ca input nu au acelasi numar de elemente, elementele lipsa vor fi inlocuite
 cu None pentru a putea fi generate max([len(x) for x in input_lists]) tuple. Pe scurt: implementati zip! (fara sa folositi zip) 😊
-'''
+"""
 
-def zip_implementation (*lists):
+
+def zip_implementation(*lists):
     max_length = 0
     for l in lists:
         if len (l) > max_length:
@@ -23,4 +24,4 @@ def zip_implementation (*lists):
 
     return tuple(result)
 
-print (zip_implementation([1,2,3], [5,6,7], ["a", "b", "c"]))
+print(zip_implementation([1,2,3], [5,6,7], ["a", "b", "c"]))
