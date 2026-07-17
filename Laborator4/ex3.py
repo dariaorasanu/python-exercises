e90a0de15e7d48a6310b231fdbfcb000c89ee66e
Daria Orasanu <dariaorasanu3@gmail.com>
2026-07-17 11:03:08 +0300

    update exercise 3

--- Laborator4/ex3.py
+++ Laborator4/ex3.py
@@ -5,15 +5,16 @@ fiecare linie continand cate o intrare din acest dictionar, sub forma cheie [tab
 import os
 
 
+
 def display_data_about_file(file_name):
     try:
         with open(file_name, 'w') as f:
             for key, value in os.environ.items():
                 f.write(f'{key}\t{value}\n')
     except OSError as e:
-        print(f'Eroare la scrierea în fișier: {str(e)}')
+        print(f'Eroare la scrierea în fisier: {str(e)}')
     except Exception as e:
-        print(f'A apărut o eroare neașteptată: {str(e)}')
+        print(f'A aparut o alta eroare: {str(e)}')
 
 
 display_data_about_file("mediu.txt")
